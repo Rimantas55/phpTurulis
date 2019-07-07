bd686badc1530fb17b4da9698ce94ae59585128c
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-07-07 14:46:37 +0300

    PDO QUERY pirmas budas - naudojant while ir fetch gaunam visus output is pasirinkto stulpelio

--- juodrastis/pdo/index.php
+++ juodrastis/pdo/index.php
@@ -14,9 +14,26 @@ $password = ''; //pass galima pasikeisti per msq Previleges-> Change password
 //$dsn = 'mysql:host='.$host.';dbname='.$dbname; //kitas uzrasymo budas
 $dsn = "mysql:host=$host; dbname=$dbname";
 $pdo = new PDO($dsn, $user, $password);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); //jei sita setAttribute() naudosim, galima resyti fetch()) rasant while ir nennaudoti fetch(PDO::FETCH_OBJ)) pvz:
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); //jei sita setAttribute() naudosim, tai kai rasysim while ten galima resyti fetch()) ir nenaudoti fetch(PDO::FETCH_OBJ)) pvz:
+//while($row = $stmt->fetch()){ //nenaudojam FETCH_OBJ bus israiska //$row->title
+	//echo $row->title . '<br>';
+//}
 
-if ($pdo) {
-	echo 'Connection working';
+// if ($pdo) {
+// 	echo 'Connection working';
+// }
+
+#PDO QUERY
+$stmt = $pdo->query('SELECT * FROM posts'); //$stmt is like $statement
+#First Method using fetch(PDO::FETCH_ASSOC)
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){ //while naudojam kai norim visa info outputinti is lenteles stulpelio
+	echo $row['author'] . '<br>';
 }
 
+//output
+// Post one 1
+// Post two 2
+// Post three 3
+// Post four 4
+// Post fifth 5
+// Post six 6
