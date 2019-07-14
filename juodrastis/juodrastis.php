203942cd530407528799261e10b0dda7e5fcfa8b
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-07-14 22:42:31 +0300

    added app files, .htaccess +  creating route useing $_GET['url']

--- juodrastis/juodrastis.php
+++ juodrastis/juodrastis.php
@@ -3,68 +3,25 @@
 //icludes some CONSTANT helpers and PHP debugging code
 include '../codingHelpers/index.php';
 
-class User {
-	protected $name;
-	protected $age;
-	
-	public function __construct($name, $age) 
-	{
-		$this->name = $name;
-		$this->age = $age;
-	}
-
-	# __get Magic method
-	public function __get($property)
-	{
-		if (property_exists($this, $property)) {
-			return $this->$property;
-		}
-	}
-
-	# __set Magic method
-	public function __set($property, $value)
-	{
-		if (property_exists($this, $property)) {
-			$this->$property = $value;
-		}
-		return $this;
-	}
-
-}
-
-
-class Customer extends User 
+class User 
 {
-	private $balance;
+	public $name;
+	public $age;
+	public static $minPasswordLenth = 6;
 
-	public function __construct($name, $age, $balance) 
+	public static function validatePassword($pass) 
 	{
-		// parent::__construct means that we do not repeat parameters from User class
-		parent::__construct($name, $age);
-		$this->balance = $balance;
-	}
-
-	public function getBalance() {
-		return $this->name . ' have paid ' . $this->balance;
-	}
-
-	//to get parameter $balance we need create in the same class new getters and setters
-	public function __get($property)
-	{
-		if (property_exists($this, $property)) {
-			return $this->$property;
+		if (strlen($pass) >= self::$minPasswordLenth) {
+			return true;
+		} else {
+			return false;
 		}
-	}
-
-	public function pay($pay)
-	{
-		echo $this->name . ' have paid ' . $pay . ' ' . $this->balance;
 	}
 }
 
-$customer1 = new Customer('John', 33, ' , you have 1000 dol. in balance');
-
-// magic method __get
-echo $customer1->__get('balance');
-echo BR;
-echo $customer1->pay(100);
+$password = 'hello'; //string lenth is 5 and pass would not be valid
+if (User::validatePassword($password)) {
+	echo 'Password Valid';
+} else {
+	echo 'Password NOT Valid';
+}
