0b0893c5c4ebd98ed9747ed94014a8082314bf1d
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-07-28 18:07:08 +0300

    in libiries folder Controller.php loads and models and views
    Per sita  Controlleri krausime views ir database

--- travercyMvc/app/controllers/Pages.php
+++ travercyMvc/app/controllers/Pages.php
@@ -1,15 +1,17 @@
 <?php
 
-class Pages 
+class Pages extends Controller
 {
 	public function __construct()
 	{
 		//echo "This is default page. <br> Pages controler loaded";
 	}
-
-	public function index()
+	
+	//defotinis page pagal method, nes Core.php mes parasem kad index paramters bus default metodas
+	public function index() 
 	{
-		//defotinis page pagal method, nes Core.php mes parasem kad index paramters bus default metodas
+		$this->view('Hello');
+
 	}
 
 	public function about($id) 
