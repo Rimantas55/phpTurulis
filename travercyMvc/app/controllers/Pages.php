756655695cb8d5df9de21e9f52b340a277981b70
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-07-28 16:53:18 +0300

    Mapping methids and parameters from url

--- travercyMvc/app/controllers/Pages.php
+++ travercyMvc/app/controllers/Pages.php
@@ -4,6 +4,16 @@ class Pages
 {
 	public function __construct()
 	{
-		echo "This is default page. <br> Pages controler loaded";
+		//echo "This is default page. <br> Pages controler loaded";
+	}
+
+	public function index()
+	{
+		//defotinis page pagal method, nes Core.php mes parasem kad index paramters bus default metodas
+	}
+
+	public function about($id) 
+	{
+		echo $id;
 	}
 }
--- travercyMvc/app/libraries/Core.php
+++ travercyMvc/app/libraries/Core.php
@@ -13,7 +13,7 @@ class Core
 
 	public function __construct()
 	{
-		print_r($this->getUrl());
+		//print_r($this->getUrl());
 
 		$url = $this->getUrl();
 
@@ -30,6 +30,25 @@ class Core
 
 		//Instanciate contriller class
 		$this->currentController = new $this->currentController;
+
+
+		//check for second part of URL, for method
+		if(isset($url[1])) {
+			//check to see if method exists in controller
+			if(method_exists($this->currentController, $url[1])) {
+				$this->currentMethod = $url[1];
+
+				// Unset 1 index
+				unset($url[1]);
+			}
+		}
+		//echo $this->currentMethod;//here we checked if we get in browser method name
+
+		// Get params
+		$this->params = $url ? array_values($url) : [];//if there is parameters it add it, if not it just will stay empty array
+
+		//Call a callback with array of params
+		call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
 	}
 
 	public function getUrl()
