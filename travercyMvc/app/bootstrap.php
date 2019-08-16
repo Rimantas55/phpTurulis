fa3d512c0f2a6a506ddc4c0e4a021a31ce83ca6c
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-08-16 21:34:55 +0300

    small updates and comments nothing special

--- travercyMvc/app/bootstrap.php
+++ travercyMvc/app/bootstrap.php
@@ -4,7 +4,7 @@
 require_once 'config/config.php';
 
 
-// Load Libraries
+// Load Libraries //ju nereikia nes susikurem autoloaderi
 // require_once 'libraries/Core.php';
 // require_once 'libraries/Controller.php';
 // require_once 'libraries/Database.php';
--- travercyMvc/app/config/config.php
+++ travercyMvc/app/config/config.php
@@ -8,12 +8,12 @@
  * Jei rasysims dirname(__FILE__) jis mums duoda -->
  * C:\xampp\htdocs\phpTurulis\travercyMvc\app\config
 
- * Jei rasysims dirname(__FILE__) jis mums duoda -->
+ * Jei rasysims dirname(dirname(__FILE__)) jis mums duoda -->
  * C:\xampp\htdocs\phpTurulis\travercyMvc\app
  */
 
 //xampp\htdocs\phpTurulis\travercyMvc\app
 define('APPROOT', dirname(dirname(__FILE__))) . '<br>';
-define('URLROOT', 'http://localhost/phpTurulis/travercyMvc'); //jei paliksim sliasa / po travercyMvc/ veliau rasant nereikes deti, bet jei nepaliksim reikes nepamirsti prirasyti
+define('URLROOT', 'http://localhost/phpTurulis/travercyMvc'); //jei paliksim sliasa / po travercyMvc/ veliau rasant viewsuose nereikes deti, bet jei nepaliksim reikes nepamirsti prirasyti
 // Site Name
 define('SITENAME', 'TravercyMVC');
